393950fc3f7a88a1428c8f87f58b5ff7b6f04ace
Fun Learn <funlearnlab@gmail.com>
2021-05-29 17:56:58 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source       = "app.terraform.io/milind-terraform-training/network/google"
   version      = "3.2.2"
   network_name = "milind-network"
-  project_id   = var.project_id
+  project_id   = var.project
   subnets = [
     {
       subnet_name   = "milind-subnet"
